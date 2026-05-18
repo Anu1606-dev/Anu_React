16a7e19ab80eb82442e4f7c49c92bdf70e541f9c
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-05-18 22:27:11 +0530

    Day 1: Initialized React project with Parcel, configured .gitignore and basic rendering setup

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const heading = React.createElement('h1', {id: "heading01"}, 'Hello from React!!❤️'); // creating element using React.createElement
-console.log(heading); // this will print the object representation of the heading element created by React.createElement
-const root = ReactDOM.createRoot(document.getElementById('root')); // creating root in ReactDOM by taking the root div
-root.render(heading); // works like appendchild in vanilla js but it replaces the content of the root div with the heading element
-// also render converts the heading object into a heading tag inside the root element in the DOM and displays it on the webpage
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div', {id: "parent"}, [ // creating multiple elemnts inside 1 single element
-    React.createElement('h1', {id: "heading01"}, 'Hello React!!❤️'),
-    React.createElement('h2', {id: "heading02"}, 'This is a subheading!!')
+const heading = React.createElement(
+  'h1',
+  { id: "heading01" },
+  'Hello from React!!❤️'
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+const parent = React.createElement('div', { id: "parent" }, [
+    React.createElement('h1', { id: "heading01", key: "h1" }, 'Hello Everyone!!❤️'),
+    React.createElement('h2', { id: "heading02", key: "h2" }, 'Hello React!!❤️')
 ]);
+
 console.log(parent);
-root.render(parent); // this will replace the previous heading element with the new parent div containing the h1 and h2 elements
+
+// This will replace previous render
+root.render(parent);
