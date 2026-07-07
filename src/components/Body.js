95caba9161ad1b8a15a98fc5ad0889d77ce275be
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-07-07 18:56:36 +0530

    feat: add top rated restaurants filter using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,25 @@
 import RestaurantCard from "./restaurantCard";
 import { resList } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-  // State Variable
-  const listOfRestaurants = resList;
+  // State Variable - Super powerful variable which remembers its value between function calls
+  const [listOfRestaurants, setListRestaurants] = useState(resList);
 
   return (
     <div className="body">
-      <div className="search">Search</div>
-
+      <div className="filter">
+        <button className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => Number(res.info.avgRatingString) > 4.5
+            );
+            setListRestaurants(filteredList);
+          }}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {/* map() creates one RestaurantCard for each restaurant */}
-
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,5 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/011/401/427/non_2x/food-signal-online-food-ordering-logo-design-order-food-on-internet-restaurant-cafe-meals-delivery-online-free-vector.jpg";
+
+// multiple export cannot be treated as default using Named Export instead of default export, we can use named export to export multiple constants from a single file. This allows us to import only the specific constants we need in other files, rather than importing the entire file. It also helps to avoid naming conflicts and makes the code more modular and easier to maintain.
