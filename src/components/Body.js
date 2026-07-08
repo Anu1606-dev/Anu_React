277a643a5c33cab4ef16684a5b7d4b009b66224e
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-07-08 18:40:21 +0530

    feat: add search bar and restaurant filtering

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   // State Variable - Super powerful variable which remembers its value between function calls
   const [listOfRestaurants, setListRestaurants] = useState([]);
-
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]); // State variable to store the filtered list of restaurants
   const [searchText, setSearchText] = useState(""); // searchText is a state variable and setSearchText is a function to update the value of searchText
 
   useEffect(() => {
@@ -23,6 +23,7 @@ const Body = () => {
 
     //optional chaining operator (?.) is used to avoid errors if any property in the chain is undefined or null
     setListRestaurants(json?.data?.cards[2]?.data?.data?.cards);// optional chaining operator (?.) is used to avoid errors if any property in the chain is undefined or null
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
   // Conditional Rendering
   // if(listOfRestaurants.length === 0) {
@@ -45,6 +46,10 @@ const Body = () => {
           <button 
           onClick={() => {
             console.log(searchText);
+            const filteredRestaurants = listOfRestaurants.filter((res) =>
+              res.data.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurants(filteredRestaurants);
           }}
           >Search</button>
         </div>
@@ -59,7 +64,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.data.id}
             resData={restaurant}
